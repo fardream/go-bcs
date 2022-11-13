e681deeef1978c163d610a7f6aa577c107cabe5d
Chao Xu <fardream@users.noreply.github.com>
2022-11-12 23:25:17 -0500

    add uleb128

---
+++ bcs/doc.go
@@ -0,0 +1,11 @@
+// bcs (binary canonical serialization) or lcs (libra canonical serialization) is developed
+// from the shuttered [libra/diem] block chain project.
+// [bcs] defines how a struct in rust-lang can be serialized into bytes, and supports features that are
+// unavaibable in golang, such as tagged union or enum.
+// By "canonical", it means the serialization is deterministic and unique.
+// On many [move-lang] based blockchains, bcs is the serialization scheme for the struct in move.
+//
+// [libra/diem]: https://www.diem.com/
+// [move-lang]: https://github.com/move-language/move
+// [bcs]: https://github.com/diem/bcs
+package bcs
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fardream/go-bcs
+
+go 1.19
