cd2d781e4de0d4655547558206ef632ac6a56e29
Aidan Woods <aidan.woods@agilebits.com>
2025-01-03 20:22:44 +0000

    Init nil pointer to support nested enums

--- bcs/enum_unmarshal_test.go
+++ bcs/enum_unmarshal_test.go
@@ -7,6 +7,13 @@ import (
 	"github.com/fardream/go-bcs/bcs"
 )
 
+type NestedEnum struct {
+	V0 *EnumExample
+	V1 *uint8
+}
+
+func (e NestedEnum) IsBcsEnum() {}
+
 func TestEnum_Unmarshal(t *testing.T) {
 	cases := [][]byte{
 		{0, 42},
@@ -16,6 +23,37 @@ func TestEnum_Unmarshal(t *testing.T) {
 
 	for _, v := range cases {
 		e := &EnumExample{}
+
+		n, err := bcs.Unmarshal(v, e)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if n != len(v) {
+			t.Errorf("want parsed length: %d, got: %d", len(v), n)
+		}
+
+		nb, err := bcs.Marshal(e)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if !slices.Equal(nb, v) {
+			t.Errorf("want %v, got %v", v, nb)
+		}
+	}
+}
+
+func TestNestedEnum_Unmarshal(t *testing.T) {
+	cases := [][]byte{
+		{0, 0, 42},
+		{1, 0},
+		{1, 42},
+		{0, 4, 3, 97, 98, 99},
+	}
+
+	for _, v := range cases {
+		e := &NestedEnum{}
 
 		n, err := bcs.Unmarshal(v, e)
 		if err != nil {
