2cd17a7c609e81fc481be7cf436ff8487bb00f85
Aidan Woods <118815907+aidantwoods-1p@users.noreply.github.com>
2025-02-10 22:39:27 +0000

    ULEB128 decoding spec conformance (#14)
    * Fail if ULEB encoding is not minimal
    
    * Error if decoded uleb128 too large
    
    * Adjust documented max values
    
    * Remove new dependency for tests

--- bcs/uleb128.go
+++ bcs/uleb128.go
@@ -6,9 +6,11 @@ import (
 	"io"
 )
 
+const MaxUleb128 = uint64(1<<32 - 1)
+
 // MaxUleb128Length is the max possible number of bytes for an ULEB128 encoded integer.
-// Go's widest integer is uint64, so the length is 10.
-const MaxUleb128Length = 10
+// All integers must fit in a u32, so the length is 10.
+const MaxUleb128Length = 5
 
 // ULEB128SupportedTypes is a contraint interface that limits the input to
 // [ULEB128Encode] and [ULEB128Decode] to signed and unsigned integers.
@@ -22,10 +24,16 @@ type ULEB128SupportedTypes interface {
 //
 // [wikipedia]: https://en.wikipedia.org/wiki/LEB128
 // [bcs]: https://github.com/diem/bcs#uleb128-encoded-integers
-func ULEB128Encode[T ULEB128SupportedTypes](input T) []byte {
-	result := make([]byte, 10)
-	i := binary.PutUvarint(result, uint64(input))
-	return result[:i]
+func ULEB128Encode[T ULEB128SupportedTypes](input T) ([]byte, error) {
+	v := uint64(input)
+
+	if v > MaxUleb128 {
+		return nil, fmt.Errorf("input %d was larger than the max allowed ULEB128", v)
+	}
+
+	result := make([]byte, MaxUleb128Length)
+	i := binary.PutUvarint(result, v)
+	return result[:i], nil
 }
 
 // ULEB128Decode decodes [io.Reader] into an integer, returns the resulted value, the number of byte read, and a possible error.
@@ -38,7 +46,7 @@ func ULEB128Decode[T ULEB128SupportedTypes](r io.Reader) (T, int, error) {
 	buf := make([]byte, 1)
 	var v, shift T
 	var n int
-	for n < 10 {
+	for n < MaxUleb128Length {
 		i, err := r.Read(buf)
 		if i == 0 {
 			return 0, n, fmt.Errorf("zero read in. possible EOF")
@@ -50,16 +58,30 @@ func ULEB128Decode[T ULEB128SupportedTypes](r io.Reader) (T, int, error) {
 
 		d := T(buf[0])
 		ld := d & 127
+
 		if (ld<<shift)>>shift != ld {
-			return v, n, fmt.Errorf("overflow at index %d: %v", n-1, ld)
+			return 0, n, fmt.Errorf("overflow at index %d: %v", n-1, ld)
 		}
 
 		ld <<= shift
 		v = ld + v
 		if v < ld {
-			return v, n, fmt.Errorf("overflow after adding index %d: %v %v", n-1, ld, v)
+			return 0, n, fmt.Errorf("overflow after adding index %d: %v %v", n-1, ld, v)
 		}
+
+		if uint64(v) > MaxUleb128 {
+			return 0, n, fmt.Errorf("overflow at index %d: %v, value does not fit in u32", n-1, ld)
+		}
+
 		if d <= 127 {
+			if shift > 0 && d == 0 {
+				return 0, n, fmt.Errorf("ULEB128 encoding was not minimal in size")
+			}
+
+			if uint64(v) > MaxUleb128 {
+				return 0, n, fmt.Errorf("overflow at index %d: %v, value does not fit in u32", n-1, ld)
+			}
+
 			return v, n, nil
 		}
 
--- bcs/uleb128_test.go
+++ bcs/uleb128_test.go
@@ -3,6 +3,7 @@ package bcs_test
 import (
 	"bytes"
 	"slices"
+	"strings"
 	"testing"
 
 	"github.com/fardream/go-bcs/bcs"
@@ -21,11 +22,15 @@ var uleb128Tests = []ULEB128Test{
 	{2097152, []byte{0x80, 0x80, 0x80, 1}},
 	{268435456, []byte{0x80, 0x80, 0x80, 0x80, 1}},
 	{9487, []byte{0x8f, 0x4a}},
+	{uint32(bcs.MaxUleb128), []byte{0xff, 0xff, 0xff, 0xff, 0x0f}},
 }
 
 func TestULEB128Encode(t *testing.T) {
 	for _, aCase := range uleb128Tests {
-		r := bcs.ULEB128Encode(aCase.Input)
+		r, err := bcs.ULEB128Encode(aCase.Input)
+		if err != nil {
+			t.Fatalf("failed to encode: %v", err)
+		}
 		if !slices.Equal(r, aCase.Expected) {
 			t.Errorf("encoding %d to %v, expecting: %v", aCase.Input, r, aCase.Expected)
 		}
@@ -52,6 +57,47 @@ func TestULEB128Decode(t *testing.T) {
 			t.Fatalf("should overflow: %d %d", r, n)
 		} else {
 			t.Logf("succeeded overflowing: %v", e)
+		}
+	}
+}
+
+func TestULEB128DecodeNonCanonical(t *testing.T) {
+	cases := [][]byte{
+		{0x80, 0},
+		{0x80, 0x80, 0},
+	}
+
+	for _, c := range cases {
+		_, _, err := bcs.ULEB128Decode[int](bytes.NewReader(c))
+		if !strings.Contains(err.Error(), "ULEB128 encoding was not minimal in size") {
+			t.Fatalf("expected failure due to non minimal input, got %v", err)
+		}
+	}
+}
+
+func TestULEB128DecodeTooLarge(t *testing.T) {
+	_, _, err := bcs.ULEB128Decode[uint64](bytes.NewReader([]byte{0x80, 0x80, 0x80, 0x80, 0x80, 0x01}))
+	if !strings.Contains(err.Error(), "failed to find most significant bytes") {
+		t.Fatalf("expected failure due to size, got %v", err)
+	}
+
+	_, _, err = bcs.ULEB128Decode[uint64](bytes.NewReader([]byte{0x80, 0x80, 0x80, 0x80, 0x10}))
+	if !strings.Contains(err.Error(), "value does not fit in u32") {
+		t.Fatalf("expected failure due to size, got %v", err)
+	}
+}
+
+func TestULEB128EncodeTooLarge(t *testing.T) {
+	cases := []uint64{
+		2 << 33,
+		2 << 36,
+		bcs.MaxUleb128 + 1,
+	}
+
+	for _, c := range cases {
+		_, err := bcs.ULEB128Encode(c)
+		if !strings.Contains(err.Error(), "larger than the max allowed ULEB128") {
+			t.Fatalf("expected failure due to size, got %v", err)
 		}
 	}
 }
